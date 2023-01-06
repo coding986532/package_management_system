6ec345744aded703d0232f392d7d6447b3c3bc1c
coding986532 <97757073+coding986532@users.noreply.github.com>
2023-01-05 21:05:31 -0800

    Update settings.py

--- package_suite/settings.py
+++ package_suite/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'package_suite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coding986532/pms_system_mt_apps', 
+        'USER': 'coding986532',
+        'PASSWORD': 'v2_3xhgi_F68dNK4ErwijzwUp4Jrvtnf',
+
+        'HOST': 'db.bit.io',
+        'PORT': '5432',
+    },
+    '''
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    '''
 }
 
 
